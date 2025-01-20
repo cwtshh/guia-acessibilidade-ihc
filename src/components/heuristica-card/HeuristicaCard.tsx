bc854c656fdc73a59b62d7ca6e44bd5edf0b67b2
cwtshh <gucosta1719@gmail.com>
2025-01-20 10:48:45 -0300

    redesign no card de heuristicas

--- src/components/heuristica-card/HeuristicaCard.tsx
+++ src/components/heuristica-card/HeuristicaCard.tsx
@@ -9,15 +9,15 @@ interface HeuristicaCardProps {
 const HeuristicaCard = ({ heuristica }: HeuristicaCardProps) => {
   return (
     <div className="card bg-base-100 shadow-xl w-full sm:w-80 md:w-96 transform transition-transform duration-300 ease-in-out hover:scale-105">
-      <div className="card-body">
+      <div className="bg-primary-800 p-4 h-24 rounded-t-lg text-white">
         <h2 className="card-title text-lg md:text-xl font-semibold">
           {`${heuristica.num} - ${heuristica.name}`}
         </h2>
+      </div>
+      <div className="card-body">
         <p className="text-sm md:text-base text-gray-700">
           {heuristica.content}
         </p>
-
-        <button className="btn bg-primary-600 mt-4">Detalhes</button>
       </div>
     </div>
   );
--- src/pages/heuristicas/Heuristicas.tsx
+++ src/pages/heuristicas/Heuristicas.tsx
@@ -74,7 +74,7 @@ const Heuristicas = () => {
           <FaSearch className="text-gray-500" />
         </label>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {filteredHeuristicas.map((heuristica, index: number) => (
             <HeuristicaCard key={index} heuristica={heuristica} />
           ))}
